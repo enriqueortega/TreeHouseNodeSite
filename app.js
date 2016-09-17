5120a4f41222333d625ea94ecaeca009d569f6b6
Enrique Ortega <enriqueortegatc@gmail.com>
2016-09-17 00:16:47 -0500

    yep

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ console.log('Server running at http://<workspace-url>/');
 
 function homeRoute(request, response){
   //if url == "/" && GET
-  console.log("weee3eeeeeeeeeesdfguirrreeeeeeeeee still reading stuff");
+  console.log("weee3eeeeeeeeeesdfguifyurrreeeeeeeeee still reading stuff");
   //if url == "/" && POST
     //redirect to /:username
 }
